e1067259d0c8a6ccf786165c3041a24e657a1941
Mayank Agarwal <mayankagarwal2120@gmail.com>
2026-07-30 19:31:03 +0530

    Add files via upload

---
+++ 3.0_Insert_Data_into_Table.sql
@@ -0,0 +1,10 @@
+SELECT * FROM employee;
+
+INSERT INTO employee(first_name, last_name, position, department, hire_date, salary)
+
+	VALUES('Mayank', 'Agarwal', 'Data Analyst', 'Analyst', '2026-11-01', 49826.92),
+		('Priya', 'Desai', 'Software Engineer', 'IT', '2025-09-20', 75000.68),
+		('Rajesh', 'Kumar', 'HR Manager', 'Human Resources', '2021-09-10', 82000.00),
+		('Sneha', 'Patel', 'Marketing Specialist', 'Marketing', '2023-05-15', 59000.91),
+		('Vikram', 'Singh', 'Sales Executive', 'Sales', '2024-02-12', 62000.72),
+		('Khushi', 'Singh', 'Software Engineer', 'IT', '2027-01-01', 36000.76);
