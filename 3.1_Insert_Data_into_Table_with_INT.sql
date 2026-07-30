4be5ef5241fefe5635000d9d45d6a4701360c2e2
Mayank Agarwal <mayankagarwal2120@gmail.com>
2026-07-30 22:49:57 +0530

    Add files via upload

---
+++ 3.1_Insert_Data_into_Table_with_INT.sql
@@ -0,0 +1,7 @@
+SELECT * FROM student;
+
+INSERT INTO student (student_id, first_name, last_name, class, roll_no)
+	VALUES (101, 'Mayank', 'Kumar', '8th', 41),
+		(102, 'Neraj', 'Sharma', '8th', 42),
+		(103, 'Raghav', 'Khanna', '8th', 43),
+		(104, 'Khushi', 'Singh', '8th', 44);
