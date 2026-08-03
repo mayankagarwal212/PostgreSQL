fb0e7774506587a050058b1c3a7832fe3b4642f1
Mayank Agarwal <mayankagarwal2120@gmail.com>
2026-08-03 23:36:21 +0530

    Modify employee table structure
    Updated the employee table schema to include a middle name and adjusted field sizes.

--- 2.0_Creating_Table.sql
+++ 2.0_Creating_Table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE employee(
 	empolyee_id SERIAL PRIMARY KEY NOT NULL,
-	first_name VARCHAR(10) NOT NULL,
-	last_name VARCHAR(10) NOT NULL,
-	position VARCHAR(50),
-	department VARCHAR(50),
+	first_name VARCHAR(15) NOT NULL,
+	middle_name VARCHAR(15),
+	last_name VARCHAR(15) NOT NULL,
+	position VARCHAR(40),
+	department VARCHAR(40),
 	hire_date DATE,
 	salary NUMERIC(10,2)
 );
