6cc832713c05ada5ac57c60856ea7ac89da3fc7b
Mayank Agarwal <mayankagarwal2120@gmail.com>
2026-08-29 19:02:09 +0530

    Add date and time functions
    Added SQL queries to retrieve current date and time, and calculate the difference in days from the added date.

--- 13.2_Date_and_Time_function.sql
+++ 13.2_Date_and_Time_function.sql
@@ -1,3 +1,10 @@
 SELECT * FROM products;
 
---
+--NOW() - Get current date and time
+SELECT NOW() AS current_datetime;
+
+--CURRENT_DATE() - Get current date  
+SELECT CURRENT_DATE AS today_date;
+
+SELECT (CURRENT_DATE - added_date) AS day_diff
+FROM products;
