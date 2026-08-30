f4c7db051ee340024f05119d52d78b8fd30b5600
Mayank Agarwal <mayankagarwal2120@gmail.com>
2026-08-30 22:05:05 +0530

    Rename day_diff to day_difference in SQL query

--- 13.2_Date_and_Time_function.sql
+++ 13.2_Date_and_Time_function.sql
@@ -6,5 +6,5 @@ SELECT NOW() AS current_datetime;
 --CURRENT_DATE() - Get current date  
 SELECT CURRENT_DATE AS today_date;
 
-SELECT (CURRENT_DATE - added_date) AS day_diff
+SELECT (CURRENT_DATE - added_date) AS day_difference
 FROM products;
